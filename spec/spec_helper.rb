11001620debf0740a115034aaacb95ef4d4b09a2
Stolbukhin Maksim <maxstbn@gmail.com>
2015-06-01 22:09:38 +0300

    модификации продуктов

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'database_cleaner'
+require 'rails/mongoid'
 
 RSpec.configure do |config|
   config.fixture_path                               = "#{Rails.root}/spec/fixtures"
